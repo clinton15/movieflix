550c9a34130d2c9f9aeee6fedce7b9af0c123e9c
clinton15 <clintonpereira97@gmail.com>
2025-01-14 00:27:17 +0530

    initial login page changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import logo from "../assets/logo.png";
 
 const Header = () => {
-  return <div className="absolute w-48 left-36">
+  return <div className="absolute w-48 left-32 z-10">
     <img src={logo} alt="netflix-logo" />
   </div>;
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,11 +3,30 @@ import bgImage from "../assets/background.jpg";
 
 const Login = () => {
   return (
-    <div>
+    <div className="bg-black h-screen z-0">
       <Header />
-      <div>
+      <div className="-z-10 opacity-50">
         <img src={bgImage} alt="background-image" />
       </div>
+      <form className="z-40 absolute w-4/12 my-36 mx-auto p-12 right-0 left-0 top-0 text-white bg-black flex-col opacity-75">
+        <h1 className="font-bold text-3xl mb-8">Sign In</h1>
+        <input
+          className="flex my-4 p-2 w-full bg-black text-white border-2 border-opacity-5 border-white-100 rounded"
+          type="input"
+          placeholder="Email address"
+        />
+        <input
+          className="flex my-4 p-2 w-full bg-black text-white border-2 border-opacity-5 border-white-100 rounded"
+          type="input"
+          placeholder="Password"
+        />
+        <input
+          type="button"
+          className="flex my-4 p-2 w-full bg-red-600 text-white cursor-pointer hover:bg-red-800 rounded"
+          value="Sign In"
+        />
+        <p>New to Netflix? <span className="font-bold text-white cursor-pointer hover:underline">Sign up now.</span></p>
+      </form>
     </div>
   );
 };
