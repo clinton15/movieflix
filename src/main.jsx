683bfbd26167c8e2a044c14bd52e25176a76bb60
clinton15 <clintonpereira97@gmail.com>
2025-02-14 00:39:44 +0530

    updated filename

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import appStore from "./utils/appstore.js";
+import appStore from "./utils/appStore.js";
 import "./i18n.js";
 
 createRoot(document.getElementById("root")).render(
